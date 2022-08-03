40b95e812d559a31ba7da9bc76ab98726a75226d
Stefano Calo <stefano.cal92@gmail.com>
2022-08-04 00:07:38 +0200

    Add sections, footer and update navbar mobile behaviour

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ const scrollFunction = function () {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         document.querySelector('#header .profile_picture').style.height = '10rem';
         document.querySelector('#header .profile_picture').style.width = '10rem';
-        document.querySelector('.navbar_name').style.opacity = '1'
-        document.querySelector('.mobile_picture').style.opacity = '1'
+        document.querySelector('.navbar_name').style.opacity = '1';
+        document.querySelector('.scroll_picture').style.opacity = '1';
+        document.querySelector('#header .name').style.fontSize = '1.5rem';
 
     } else {
         document.querySelector('#header .profile_picture').style.height = '20rem';
         document.querySelector('#header .profile_picture').style.width = '20rem';
-        document.querySelector('.navbar_name').style.opacity = '0'
-        document.querySelector('.mobile_picture').style.opacity = '0'
+        document.querySelector('.navbar_name').style.opacity = '0';
+        document.querySelector('.scroll_picture').style.opacity = '0';
+        document.querySelector('#header .name').style.fontSize = '3rem';
     }
 }
 
@@ -27,11 +29,9 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 })
 
-document.querySelectorAll('.nav_item').forEach( n => {
+document.querySelectorAll('.nav_item').forEach(n => {
     n.addEventListener('click', () => {
-        navMenu.classList.remove('active');
         hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
     })
-    }
-
-)
+})
